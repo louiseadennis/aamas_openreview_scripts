2edbedfe3da84f29c2bc0205f32839f752d5be79
t95229ld <louise.dennis@manchester.ac.uk>
2025-08-02 13:26:05 +0100

    Client Object

---
+++ client_object.py
@@ -0,0 +1,10 @@
+import openreview
+
+# API V2
+client = openreview.api.OpenReviewClient(
+    baseurl='https://devapi2.openreview.net',
+    username='Your UserName Here',
+    password='Your Password Here'
+)
+
+
