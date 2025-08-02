fa9f3d363a17a43ae367f22f4793fd9fdbc2da60
t95229ld <louise.dennis@manchester.ac.uk>
2025-08-02 14:01:04 +0100

    Deleteing Reviews.

--- get_all_metareviews.py
+++ get_all_metareviews.py
@@ -5,7 +5,7 @@ client = client_object.client
 
 venue_id = 'AAMAS/2026/Test'
 
-reply_type = "Official_Review" #also: "Meta_Review","Official_Comment", "Decision", "Rebuttal" etc.
+reply_type = "Meta_Review" #also: "Meta_Review","Official_Comment", "Decision", "Rebuttal" etc.
 submissions = client.get_all_notes(invitation=f'{venue_id}/-/Submission',details='replies')
 
 replies = [reply for submission in submissions for reply in submission.details['replies'] if any(invitation.endswith(reply_type) for invitation in reply['invitations'])]
