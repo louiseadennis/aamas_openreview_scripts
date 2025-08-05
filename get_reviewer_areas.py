06e79879f4eaf3fce5089fadbab0069680a3377c
t95229ld <louise.dennis@manchester.ac.uk>
2025-08-05 12:07:57 +0100

    Getting Reviewer areas.

--- get_reviewer_areas.py
+++ get_reviewer_areas.py
@@ -6,6 +6,7 @@ venue_id = client_object.venue_id
 
 registration_forum = 'vSikVyOdps' #found this id by running the get_venue_info script.
 
+
 # Get all replies to the registration forum
 notes = client.get_all_notes(replyto=registration_forum)
 
@@ -15,13 +16,11 @@ for n in notes:
     signature = n.signatures[0]
     profile = client.get_profile(signature)
     profile_id = profile.id
-    # print(n)
-    try:
-        registrations[profile_id] = n.content['area']['value']
-    except:
-        print(profile_id)
-        print("no area")
-
-for reviewer_id, subject in registrations.items():
-    print(reviewer_id)
-    print(subject)
+    if (not profile_id in registrations):
+        try:
+            print(profile_id)
+            print(n.content['area']['value'])
+        except:
+            print(profile_id)
+            print("no area")
+    
