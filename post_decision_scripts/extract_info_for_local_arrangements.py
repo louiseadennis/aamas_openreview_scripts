46545123456694a87197b6ce3cd19f56fd16c23d
t95229ld <louise.dennis@manchester.ac.uk>
2026-01-25 19:58:06 +0000

    Updating author info.

--- post_decision_scripts/extract_info_for_local_arrangements.py
+++ post_decision_scripts/extract_info_for_local_arrangements.py
@@ -11,9 +11,9 @@ submissions = client.get_all_notes(invitation=f'{venue_id}/-/Submission',details
 
 
 for submission in submissions:
-     if (not f'{venue_id}/-/Desk_Rejected_Submission' in submission.invitations):
+             #if (not f'{venue_id}/-/Desk_Rejected_Submission' in submission.invitations):
         # print(note)
-            if (not f'{venue_id}/-/Withdrawn_Submission' in submission.invitations):
+                  #  if (not f'{venue_id}/-/Withdrawn_Submission' in submission.invitations):
                 decision = ""
                 
                 for reply in submission.details['replies']:
@@ -26,22 +26,34 @@ for submission in submissions:
                     author_ids = submission.content['authorids']['value']
                     profiles = openreview.tools.get_profiles(client, author_ids)
                     author_names = []
+                    author_list = []
+                    author_id_to_name = {}
+                    author_id_to_email = {}
                     for profile in profiles:
-                        names = profile.content['names']
-                        #print(names)
-                        fullname = ""
-                        for name in names:
-                            try:
-                                if name['preferred']:
-                                    fullname = name['fullname']
-                            except:
-                                fullname = name['fullname']
-                        if fullname == "":
-                            #print(profile)
-                            fullname = names[0]['fullname']
-                        #print(fullname)
-                        new_string = f'\"{fullname}\",'
+                        found_name = False
+                                    # print(profile)
+                        username = profile.id
+                        if (username not in author_ids):
+                            for name in profile.content['names']:
+                                if name['username'] in author_ids:
+                                    username = name['username']
+                        for name in profile.content['names']:
+                            if ('preferred' in name.keys() and name['preferred']):
+                                #print("adding1 " + username)
+                                author_id_to_name[username] = name['fullname']
+                                found_name = True
+                                break
+                        if (not found_name):
+                            #print("adding " + username)
+                            author_id_to_name[username] = profile.content['names'][0]['fullname']
                         author_email = profile.get_preferred_email()
+                        author_id_to_email[username] = author_email
+
+                    for id in author_ids:
+                        fullname = author_id_to_name[id]
+                        author_email = author_id_to_email[id]
+
+                        new_string = f'\"{fullname}\",'
                         email_string = f'\"{author_email}\"'
                         author_names.append(fullname)
                         author_names.append(email_string)
